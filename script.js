328003d7505de8cb205ac39e3eb9dff23ddba155
Adam Więcek <adamw28@gmail.com>
2018-07-23 02:44:04 +0200

    Remove necessary code

--- script.js
+++ script.js
@@ -27,19 +27,19 @@ $(document).ready(function() {
       for (let i = 0; i < squares.length; i++) {
         if((squares[i] === "X" || squares[i] === "O") && ((i+1)%50 != 0 && (i+2)%50 != 0 && (i+3)%50 != 0 && (i+4)%50 != 0)){
           if(squares[i] === squares[i+1] && squares[i] === squares[i+2] && squares[i] === squares[i+3] && squares[i] === squares[i+4]){
-            score = [i,i+1,i+2,i+3,i+4,squares[i]];console.log(1);
+            score = [i,i+1,i+2,i+3,i+4,squares[i]];
             return score;
           }
           if(squares[i] === squares[i+50] && squares[i] === squares[i+100] && squares[i] === squares[i+150] && squares[i] === squares[i+200]){
-            score = [i,i+50,i+100,i+150,i+200,squares[i]];console.log(2);
+            score = [i,i+50,i+100,i+150,i+200,squares[i]];
             return score;
           }
           if(squares[i] === squares[i+51] && squares[i] === squares[i+102] && squares[i] === squares[i+153] && squares[i] === squares[i+204]){
-            score = [i,i+51,i+102,i+153,i+204,squares[i]];console.log(3);
+            score = [i,i+51,i+102,i+153,i+204,squares[i]];
             return score;
           }
           if(squares[i] === squares[i+49] && squares[i] === squares[i+98] && squares[i] === squares[i+147] && squares[i] === squares[i+196]){
-            score = [i,i+49,i+98,i+147,i+196,squares[i]];console.log(4);
+            score = [i,i+49,i+98,i+147,i+196,squares[i]];
             return score;
           }
         }
@@ -56,7 +56,7 @@ $(document).ready(function() {
         score = changeSquare($(this));
       }
 
-      if(score){console.log(score);
+      if(score){
         $(".game").text("Winner: " + score[5]);
         $(".square").attr("disabled","disabled");
         let [a,b,c,d,e] = score;
@@ -69,8 +69,6 @@ $(document).ready(function() {
       else{
         $(".game").text("Next player: " + (isXNext ? "X" : "O"));
       }
-
-      console.log($(this).attr("id"),squares[$(this).attr("id")]);
     });
   });
   
